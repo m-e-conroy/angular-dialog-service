32d56b02c32bc63e244e313df411ca9fd8e4b533
zhuzhichao <me@zhuzhichao.com>
2015-03-11 22:59:27 +0800

    add Simplified Chinese
    Add Simplified Chinese dialogs-default-translations.js  .

--- src/dialogs-default-translations.js
+++ src/dialogs-default-translations.js
@@ -46,6 +46,23 @@
             DIALOGS_NO: "No"
         });
 
+        $translateProvider.translations('zh-CN',{
+          DIALOGS_ERROR: "错误",
+          DIALOGS_ERROR_MSG: "出现未知错误。",
+          DIALOGS_CLOSE: "关闭",
+          DIALOGS_PLEASE_WAIT: "请稍候",
+          DIALOGS_PLEASE_WAIT_ELIPS: "请稍候...",
+          DIALOGS_PLEASE_WAIT_MSG: "请等待操作完成。",
+          DIALOGS_PERCENT_COMPLETE: "% 已完成",
+          DIALOGS_NOTIFICATION: "通知",
+          DIALOGS_NOTIFICATION_MSG: "未知应用程序的通知。",
+          DIALOGS_CONFIRMATION: "确认",
+          DIALOGS_CONFIRMATION_MSG: "确认要求。",
+          DIALOGS_OK: "确定",
+          DIALOGS_YES: "确认",
+          DIALOGS_NO: "取消"
+        });
+
         $translateProvider.translations('es',{
           DIALOGS_ERROR: "Error",
           DIALOGS_ERROR_MSG: "Se ha producido un error.",
